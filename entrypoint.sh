eb7fc55c543a6d03f2a173211ee22f651e20a3d9
Matteo <matteo.gallo@tuta.io>
2020-09-28 14:41:35 +0100

    Removing host access. IntelliJ will have default access only to ~/IdeaProjects. Using OpenJDK 11 extension for the IDE and embedded shell. Configuring embedded Maven to work out of the box with the embedded shell. Maven local repository will be in /var/data/.m2/repository/. Plugins, system and config IntelliJ folders are in /var/data/. Logs are in /var/cache/log folder.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
-set -o errexit
-
-IDEA_JDK="/app/idea-IC/jre64"
-export IDEA_JDK
-TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
-export TMPDIR
+export JAVA_HOME=/app/jdk
+export JAVA_TOOL_OPTIONS=-Djava.io.tmpdir=${XDG_CACHE_HOME}/tmp
+export MAVEN_HOME=/app/idea-IC/plugins/maven/lib/maven3
+export M2_REPO=/var/data/.m2/repository
+export PATH=$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin
+export IDEA_JDK=/app/jdk
+export TMPDIR=${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}
+export IDEA_PROPERTIES=/app/bin/idea.properties
 
 exec env /app/idea-IC/bin/idea.sh "$@"
