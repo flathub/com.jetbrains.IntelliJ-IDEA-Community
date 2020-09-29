d56b91895d5587192ef77303874d8b2d517b2b51
Matteo <matteo.gallo@tuta.io>
2020-09-29 13:21:11 +0100

    Removing idea.properties and its references in json file and entrypoint. Properties customizations are not needed since latest IntelliJ builds.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,5 @@ export M2_REPO=/var/data/.m2/repository
 export PATH=$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin
 export IDEA_JDK=/app/jdk
 export TMPDIR=${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}
-export IDEA_PROPERTIES=/app/bin/idea.properties
 
 exec env /app/idea-IC/bin/idea.sh "$@"
