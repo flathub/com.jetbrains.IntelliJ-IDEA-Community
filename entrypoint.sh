a22937f72887270001c0c77b45fdbf4b23780ad3
Matteo <matteo.gallo@tuta.io>
2020-09-30 16:20:22 +0100

    Removing commented out code.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 export JAVA_HOME=/app/jdk
 export JAVA_TOOL_OPTIONS=-Djava.io.tmpdir=${XDG_CACHE_HOME}/tmp
 export MAVEN_HOME=/app/idea-IC/plugins/maven/lib/maven3
-# export M2_REPO=/var/data/.m2/repository
 export PATH=$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin
 export IDEA_JDK=/app/jdk
 export TMPDIR=${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}
