ae17ac3e87c983de00b70315b02d782fe96c08ca
Matteo <matteo.gallo@tuta.io>
2020-10-09 01:09:38 +0100

    Adding java.io.tmpdir to idea64.vmoptions.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
 export JAVA_HOME=/app/jdk
-export JAVA_TOOL_OPTIONS=-Djava.io.tmpdir=${XDG_CACHE_HOME}/tmp
 export MAVEN_HOME=/usr/lib/sdk/openjdk11/maven
 export ANT_HOME=/usr/lib/sdk/openjdk11/ant
 export GRADLE_HOME=/usr/lib/sdk/openjdk11/gradle
 export PATH=$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin:$ANT_HOME/bin:$GRADLE_HOME/bin
-export IDEA_JDK=/app/jdk
 export TMPDIR=${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}
+
+export IDEA_JDK=/app/jdk
+export IDEA_VM_OPTIONS=/app/bin/idea64.vmoptions
 
 exec env /app/idea-IC/bin/idea.sh "$@"
