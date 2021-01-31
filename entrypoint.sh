312346a61099da9199d2307c5a8a402df76d0060
x80486 <masked.github@mailnull.com>
2021-01-31 18:28:06 -0500

    revert: reuse default provided idea64.vmoptions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env sh
 
-export JAVA_HOME=/app/jdk
-export MAVEN_HOME=/usr/lib/sdk/openjdk11/maven
 export ANT_HOME=/usr/lib/sdk/openjdk11/ant
 export GRADLE_HOME=/usr/lib/sdk/openjdk11/gradle
-export PATH=$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin:$ANT_HOME/bin:$GRADLE_HOME/bin
-export TMPDIR=${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}
-
 export IDEA_JDK=/app/jdk
-export IDEA_VM_OPTIONS=/app/bin/idea64.vmoptions
+export JAVA_HOME=/app/jdk
+export MAVEN_HOME=/usr/lib/sdk/openjdk11/maven
+export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
+export PATH="${PATH}:${ANT_HOME}/bin:${GRADLE_HOME}/bin:${JAVA_HOME}/bin:${MAVEN_HOME}/bin"
 
-exec env /app/idea-IC/bin/idea.sh "$@"
+exec /app/idea-IC/bin/idea.sh "$@"
