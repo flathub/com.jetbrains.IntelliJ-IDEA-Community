48d7a89eb8bc0473cc0e92b5802b65be7dd8b848
Matteo Gallo <matteo.gallo@tuta.io>
2021-01-31 21:26:59 +0000

    refactor: sandboxing improvements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
-set -o errexit
+export JAVA_HOME=/app/jdk
+export MAVEN_HOME=/usr/lib/sdk/openjdk11/maven
+export ANT_HOME=/usr/lib/sdk/openjdk11/ant
+export GRADLE_HOME=/usr/lib/sdk/openjdk11/gradle
+export PATH=$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin:$ANT_HOME/bin:$GRADLE_HOME/bin
+export TMPDIR=${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}
 
-IDEA_JDK="/app/idea-IC/jre64"
-export IDEA_JDK
-TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
-export TMPDIR
+export IDEA_JDK=/app/jdk
+export IDEA_VM_OPTIONS=/app/bin/idea64.vmoptions
 
 exec env /app/idea-IC/bin/idea.sh "$@"
