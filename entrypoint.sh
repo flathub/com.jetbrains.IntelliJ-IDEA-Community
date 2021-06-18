359d01d4d0d7493874d8b1da096ea11f81346cee
Jackson Dearnley <jackson.dearnley@gmail.com>
2021-06-18 17:53:46 -0400

    feat: add support for discord integration

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,9 @@ for i in {0..9}; do
     test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
 done
 
+# https://plugins.jetbrains.com/plugin/10233-discord-integration
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
 exec env /app/idea-IC/bin/idea.sh "$@"
