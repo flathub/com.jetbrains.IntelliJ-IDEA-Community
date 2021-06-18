9559a149afb2734a3d5ab84c5c6d66437e4a1ed1
Jackson Dearnley <jackson.dearnley@gmail.com>
2021-06-18 16:09:21 -0400

    Add support for Discord Integration

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,9 @@ export IDEA_JDK
 TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 export TMPDIR
 
+# https://plugins.jetbrains.com/plugin/10233-discord-integration
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
 exec env /app/idea-IC/bin/idea.sh "$@"
