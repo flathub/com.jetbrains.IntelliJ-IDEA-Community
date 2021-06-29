afca4230a21ae147ec0d5434dae8a910ecdbe8ec
Matteo Gallo <matteo.gallo@tuta.io>
2021-06-29 20:54:46 +0000

    chore: updating beta channel with latest changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
-export ANT_HOME=/usr/lib/sdk/openjdk11/ant
-export GRADLE_HOME=/usr/lib/sdk/openjdk11/gradle
-export IDEA_JDK=/app/jdk
 export JAVA_HOME=/app/jdk
 export MAVEN_HOME=/usr/lib/sdk/openjdk11/maven
-export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
-export PATH="${PATH}:${ANT_HOME}/bin:${GRADLE_HOME}/bin:${JAVA_HOME}/bin:${MAVEN_HOME}/bin"
+export ANT_HOME=/usr/lib/sdk/openjdk11/ant
+export GRADLE_HOME=/usr/lib/sdk/openjdk11/gradle
+export PATH=$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin:$ANT_HOME/bin:$GRADLE_HOME/bin
+export TMPDIR=${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}
 
-exec /app/idea-IC/bin/idea.sh "$@"
+# https://plugins.jetbrains.com/plugin/10233-discord-integration
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
+exec env /app/idea-IC/bin/idea.sh "$@"
