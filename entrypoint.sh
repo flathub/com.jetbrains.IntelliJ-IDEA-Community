51f0243b39e22bc2258038341593906fafc82eb1
Matteo <matteo.gallo@tuta.io>
2021-06-29 17:18:51 -0300

    Removing idea64.vmoptions. Removing IDEA_JDK and IDEA_VM_OPTIONS setting in entrypoint.sh to use internal JDK to run IntelliJ. Fixing indentation in yml file.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,6 @@ export GRADLE_HOME=/usr/lib/sdk/openjdk11/gradle
 export PATH=$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin:$ANT_HOME/bin:$GRADLE_HOME/bin
 export TMPDIR=${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}
 
-export IDEA_JDK=/app/jdk
-export IDEA_VM_OPTIONS=/app/bin/idea64.vmoptions
-
 # https://plugins.jetbrains.com/plugin/10233-discord-integration
 for i in {0..9}; do
     test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
